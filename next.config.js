752c6f5507ce138645c48c31647800a84e4155d4
hyunjun-kim <khyunjun@pulleymath.com>
2022-02-21 18:13:48 +0900

    pwa 세팅 작업

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+const runtimeCaching = require("next-pwa/cache");
+module.exports = withPWA({
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === 'development',
+  },
+})
