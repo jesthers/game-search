a304fec46e1107c1ff1b7235f7548aeb055f1312
kimik-hyum <familykim01@naver.com>
2022-03-20 20:20:38 +0900

    eslint 및 기본 라이브러리들 추가작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          refetchOnReconnect: false,
+          retry: false,
+        },
+      },
+    })
+  )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 export default MyApp
